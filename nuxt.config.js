e0bfaff3bbcc7b64ce16da0b4104ff5700e0070f
Arsen <joker5916@riseup.net>
2021-02-01 17:47:46 +0300

    PWA установлен

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,6 +32,7 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
+    '@nuxtjs/pwa',
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
@@ -51,5 +52,8 @@ export default {
   ],
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+  },
+  pwa: {
+    icon: false // disables the icon module
   }
 }
