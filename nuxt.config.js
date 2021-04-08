1e7adda6111340402869ee07967525983680af78
Arsen <joker5916@riseup.net>
2021-04-08 16:58:23 +0300

    Немного seo

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   ssr: false,
-  target:'static',
+  target:'universal',
   head: {
     title: 'Кооператив Noda-IT',
     meta: [
